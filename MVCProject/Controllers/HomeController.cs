c59e6d15f21d0019f7867a0b1b9019a35acf531b
Andrew Ahverdov <wm-andrew@mail.ru>
2023-02-05 20:11:28 +0300

    Добавьте файлы проекта.

---
+++ MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using System.Diagnostics;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {            
+            return View(DataBookContext.GetMyDB());
+        }
+
+        public IActionResult ConcreteDataBook(int dataBook)
+        {
+            return View(DataBookContext.GetByID(dataBook));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCProject/Models/DataBookContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Linq;
+
+namespace MVCProject.Models
+{
+    public class DataBookContext
+    {
+        /// <summary>
+        /// Данные.
+        /// </summary>
+        public static List<DataBook> MyData { get; set; } = new List<DataBook>();
+
+        static DataBookContext()
+        {
+            GenerateDB();
+        }
+
+        /// <summary>
+        /// Создать тестовую базу данных.
+        /// </summary>
+        /// <returns>Список с данными</returns>
+        public static void GenerateDB()
+        {
+            for (int i = 1; i <= 100; i++)
+                MyData.Add(new DataBook());
+        }
+
+        /// <summary>
+        /// Получить базу данных.
+        /// </summary>
+        /// <returns>Список с данными</returns>
+        public static List<DataBook> GetMyDB()
+        {
+            return MyData;
+        }
+
+        /// <summary>
+        /// Получить запись по Id.
+        /// </summary>
+        /// <returns>DataBook</returns>
+        public static DataBook GetByID(int id)
+        {
+            return MyData.FindAll(d => d.ID == id).FirstOrDefault();
+        }
+    }
+}
